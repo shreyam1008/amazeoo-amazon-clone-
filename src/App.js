83232312d5cc7ca78037a30e699e9376b5794fea
Shreyam Adhikari <shreyam1008@gmail.com>
2020-10-29 14:38:10 +0545

    initilizing router for pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import React from "react";
 import Header from "./Header";
 import Home from "./Home";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div className="app">
-      {/* header */}
-      <Header />
-
-      {/* home-everything */}
-      <Home />
-    </div>
+    // for pages like. checkout. basket
+    <Router>
+      <div className="app">
+        {/* on which link what happens check */}
+        <Switch>
+          {/* to go to checkout page */}
+          <Route path="/checkout">
+            <Header />
+            <h1>checkout</h1>
+          </Route>
+          {/* "default route by "/" ..use in last*/}
+          <Route path="/">
+            {/* header */}
+            <Header />
+            {/* home-everything */}
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
