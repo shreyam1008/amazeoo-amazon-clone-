aea091d5bc6dfab329191a772a0f5b8775d627ec
Shreyam Adhikari <shreyam1008@gmail.com>
2020-10-29 07:41:21 +0545

    setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDqlscmmJu7SI9hF4EqyEDJNIJLyP_SYhQ",
+  authDomain: "amazeoo.firebaseapp.com",
+  databaseURL: "https://amazeoo.firebaseio.com",
+  projectId: "amazeoo",
+  storageBucket: "amazeoo.appspot.com",
+  messagingSenderId: "804730922195",
+  appId: "1:804730922195:web:cb5a24d22e684933a8a026",
+  measurementId: "G-6FX0W7D3CD",
+};
